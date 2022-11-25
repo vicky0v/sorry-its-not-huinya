03c31fe7b22919c729df98ded423b692c2188c54
VR <Blazor.com>
2022-11-25 15:07:37 +0300

    something_6

--- Blazor.Learner/Blazor.Learner/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Authentication/Register.razor.g.cs
+++ Blazor.Learner/Blazor.Learner/Client/obj/Debug/net5.0/RazorDeclaration/Pages/Authentication/Register.razor.g.cs
@@ -13,91 +13,91 @@ namespace Blazor.Learner.Client.Pages.Authentication
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 1 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 2 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 3 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 4 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 5 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 6 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 7 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 8 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 9 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Blazor.Learner.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 10 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using Blazor.Learner.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
+#line 12 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
+#line 2 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
 using Blazor.Learner.Client.Services;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
+#line 3 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
 using Blazor.Learner.Shared.Models;
 
 #line default
@@ -113,7 +113,7 @@ using Blazor.Learner.Shared.Models;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 35 "C:\Users\truha\Desktop\Xiaomi Redmi_7 huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
+#line 35 "C:\Users\truha\Desktop\huinya\Blazor.Learner\Blazor.Learner\Client\Pages\Authentication\Register.razor"
             
 
     RegisterRequest registerRequest { get; set; } = new RegisterRequest();
---
+++ Blazor.Learner/Blazor.Learner/Shared/Models/Dev.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Learner.Shared.Models
+{
+    public class Dev
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public decimal Experience { get; set; }
+    }
+}
